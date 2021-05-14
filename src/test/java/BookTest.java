93631f1d44514a917295f3edd2362aab24c85cb7
Felix Wolf <x3mdroid@gmail.com>
2021-05-14 11:37:21 +0200

    add documentation to test methods

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -6,39 +6,60 @@ import library.assistant.database.DatabaseHandler;
 import library.assistant.ui.listbook.BookListController;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static library.assistant.database.SQLStatements.setBookAvailability;
+import static org.junit.Assert.*;
 
 public class BookTest {
+    // generic book id
     final String id = "134565432135";
+    // test-book
     final Book bookModel = new Book(id, "",  "", "", true);
+    // test-book of a different type that is equal to bookModel in values
     final BookListController.Book bookControllerModel = new BookListController.Book("", id, "", "", true);
 
+    /**
+     * wipes the database before a new test run
+     */
     public BookTest() {
-        // WIPE DATABASE
         DataHelper.wipeTable("Issue");
         DataHelper.wipeTable("Member");
         DataHelper.wipeTable("Book");
         System.out.println("Test Database wiped");
     }
 
+    /**
+     * tests book insertion
+     */
     @Test
     public void insertBookTest() {
         assertTrue(DataHelper.insertNewBook(bookModel));
     }
 
+    /**
+     * tests if a book exists after it was inserted into the database
+     */
     @Test
     public void bookExistsTest() {
         DataHelper.insertNewBook(bookModel);
         assertTrue(DataHelper.isBookExists(id));
     }
 
+    /**
+     * tests book deletion
+     */
     @Test
     public void deleteBookTest() {
         DataHelper.insertNewBook(bookModel);
         assertTrue(DatabaseHandler.getInstance().deleteBook(bookControllerModel));
     }
 
+    /**
+     * tests whether information can be retrieved from an
+     * inserted book
+     */
     @Test
     public void getBookInfoTest() {
         DataHelper.insertNewBook(bookModel);
